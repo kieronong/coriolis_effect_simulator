bb38c0be506a30b8569b00191f0dd134e797bd3d
Kieron Ong <kieronong@berkeley.edu>
2021-09-26 16:53:20 -0700

    done w skeleton! texture and explanation left

--- sketch.js
+++ sketch.js
@@ -11,17 +11,20 @@ let og_mouse_y;
 let slider;
 let mouse_triggered = false;
 let draw_plane = false;
+let drop_dots = false;
+let dropped_dots = [];
+let drop_dots_start_framecount;
 
-function preload (){
+function preload(){
   earth_texture=loadImage('assets/earthtexture.jpg');
   paper_plane = loadImage('assets/paperplane.png');
 }
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(windowWidth, windowHeight - 110, WEBGL);
   slider = createSlider(-1, 1, -1, 1);
-  slider.position(10, 10);
+  slider.position(10, 110);
   slider.style('width', '200px');
 }
 
@@ -30,40 +33,69 @@ function draw() {
   angleMode(DEGREES);
 
   push()
-  x_rotation += (0.1*x_offset);
-  y_rotation += (0.1*(slider.value()+y_offset));
-  z_rotation += (0.1*z_offset);
-  rotateX(x_rotation);
+  // x_rotation += (0.05*x_offset);
+  y_rotation += (0.1*slider.value());
+  // rotateX(x_rotation);
   rotateY(y_rotation);
-  rotateZ(z_rotation);
   texture(earth_texture);
-  sphere(260,25,25);
+  sphere(240,25,25);
   pop()
 
-  if (draw_plane) {
-    rotateY(0);
-    translate(0, 0, 260);
-    texture(paper_plane);
-    box(40, 40, 0);
+  if (drop_dots) {
+    if ((frameCount - drop_dots_start_framecount) % 80 == 0) {
+      dropped_dots.push([x_rotation, y_rotation]);
+      if (dropped_dots.length > 8) {
+        dropped_dots.shift()
+      }
+    }
+    for (let d of dropped_dots) {
+      push()
+      rotateY(y_rotation);
+      rotateY(-d[1]);
+      rotateX(d[0]);
+      translate(0, 0, 250);
+      texture(paper_plane);
+      box(5, 5, 0);
+      pop()
+    }
   }
 
-  if (mouse_triggered) {
+  if (draw_plane) {
+    push()
+    x_rotation += (0.1*x_offset);
+    rotateX(x_rotation);
+    translate(0, 0, 250);
+    texture(paper_plane);
+    box(20, 20, 0);
+    pop()
     
   }
 
-  orbitControl();
-}
+  // N key
+  if (keyIsDown(78)) {
+    drop_dots_start_framecount = frameCount
+    draw_plane = true;
+    drop_dots = true;
+    x_offset = 1;
+  }
 
-function mousePressed() {
-  mouse_triggered = true;
-  draw_plane = true;
-  og_mouse_x = mouseX;
-  og_mouse_y = mouseY;
-}
+  // S key
+  if (keyIsDown(83)) {
+    drop_dots_start_framecount = frameCount
+    draw_plane = true;
+    drop_dots = true;
+    x_offset = -1;
+  }
 
-function mouseReleased() {
-  let x_travel = mouseX - og_mouse_x
-  let y_travel = mouseY - og_mouse_y
-  y_offset = y_travel / 200
-  z_offset = x_travel / 200
+  // X key
+  if (keyIsDown(88)) {
+    draw_plane = false;
+    drop_dots = false;
+    dropped_dots = [];
+    x_rotation = 0;
+    x_offset = 0
+    y_rotation = 0
+  }
+
+  orbitControl()
 }
